01e93b8451ea5985d06b768e1b352619d52a639f
Kawsar Ibn Siraj <38612699+kawsarIbnSiraj@users.noreply.github.com>
2018-09-18 20:56:01 +0600

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-
-
 /**
  * - todo list app
  * - created by kawsar Ibn Siraj
@@ -13,36 +11,54 @@
  var input = document.getElementById('input');
  var addBtn = document.getElementById('addBtn');
  var list = document.getElementById('list');
- var listArry = [];
+ var listArry = [
+     'type new task and then click to add button',
+     'This example demonstrates how to create a todo list app using pure javascript'
+ ];
+
+
+
+// initialize addItem function
+addItem(listArry);
+
+// create addTodo function
+function addItem(listArry){
+    // define empty str
+    var str = "";
+    
+    // loop in listArry
+    // value store in str
+    listArry.forEach(function (value, key) {
+        // store data in str
+        str += "<li class='list-group-item border'>" + value + "<button onclick='removeItem(this.parentNode)' class='btn btn-danger ti-close float-right'></button></li>";
+    });
+    
+    // append to markup
+    list.innerHTML = str;
+}
 
  // click event of add button
  addBtn.addEventListener('click', function () {
-
+    // check input value
     if (input.value == '' || null ) {
         alert('please input something below the input box');
     }
     else {
         listArry.push(input.value);
         input.value = '';
-
-        var str = "";
-        // loop for all value in array store
-        listArry.forEach(function (value, key) {
-            str += "<li class='list-group-item border'>" + value + "<span class='btn btn-danger ti-close float-right'></span> </li>";
-        });
         
-        // append to markup
-        list.innerHTML = str;
-
-        var dlt = list.querySelectorAll('span');
-
-        // specific item delete in click event
-        for (let i = 0; i < dlt.length; i++) {
-            dlt[i].addEventListener('click', function () {
-                this.parentNode.remove();
-                listArry.shift(this.parentNode);
-            });
-        }
+        // call addTodo function
+        addItem(listArry);
     }
+});
 
- });
+
+// create removeItem function
+function removeItem(item) {
+    // item remove
+   item.remove();
+    
+   // item remove form listArry
+   listArry.shift(item.parentNode);
+    
+}
